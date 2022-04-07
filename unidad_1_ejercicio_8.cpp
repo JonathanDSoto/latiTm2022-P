e43a4479c552839380dc045689a0c98237bd9676
Jonathan Soto <jonathansoto@iMac-de-Jonathan.local>
2022-04-07 11:19:12 -0600

    ejercicio 8

---
+++ unidad_1_ejercicio_8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    char codigo = 0;
+
+    cout << "Ingrese su identificador de usuario: "<<endl;
+
+    cin >> codigo;
+
+    cout << "Bienvenido usuario: " << codigo << endl;
+
+    return 0;
+}
